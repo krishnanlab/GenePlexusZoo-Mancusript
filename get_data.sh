010a03ecee4f3523fd52623ffbb73155e0453a70
ChristopherMancuso <chrismancuso1984@gmail.com>
2023-10-05 13:40:24 -0400

    updared how Zenodo data is downloaded

--- get_data.sh
+++ get_data.sh
@@ -1,15 +1,12 @@
 home_dir=$(dirname $0)
 cd $home_dir
 
-rm -rf "GenePlexusZooManuscript.tar.gz"*
+# echo remove older versions first
+rm -rf GenePlexusZooManuscript.tar.gz
+rm -rf data
+rm -rf results
+rm -rf figures
 
-if [ -d data ]; then
-	echo cleaning up old data
-	rm -rf ./data/*
-else
-	mkdir data
-fi
-
-wget https://zenodo.org/record/7888044/files/GenePlexusZooManuscript.tar.gz
-tar -xvzf "GenePlexusZooManuscript.tar.gz" -C data --strip-components=1
-rm -f "GenePlexusZooManuscript.tar.gz"
+wget https://zenodo.org/record/8411134/files/GenePlexusZooManuscript.tar.gz
+tar -xvzf "GenePlexusZooManuscript.tar.gz" --strip-components=1
+rm -rf "GenePlexusZooManuscript.tar.gz"
